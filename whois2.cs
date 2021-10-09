4dff426e2803b3df4a2bd67547a818b6514693cc
forkiesassds <54495659+forkiesassds@users.noreply.github.com>
2021-10-09 19:26:47 +0300

    add more info to whois2

--- whois2.cs
+++ whois2.cs
@@ -34,6 +34,9 @@ namespace MCGalaxy.Commands.Info
 		public override string type { get { return CommandTypes.Information; } }
 		public override bool UseableWhenFrozen { get { return true; } }
 		public override LevelPermission defaultRank { get { return LevelPermission.Guest; } }
+		public override CommandPerm[] ExtraPerms {
+            get { return new[] { new CommandPerm(LevelPermission.AdvBuilder, "can see player's IP and if on whitelist") }; }
+        }
 		public override CommandAlias[] Aliases {
             get { return new CommandAlias[] { new CommandAlias("Info"), new CommandAlias("i") }; }
         }
@@ -61,10 +64,26 @@ namespace MCGalaxy.Commands.Info
 				p.Message("&S-[ {0} &S({1}) &S(&coffline&S) ]-", fullName, target.Name);
 				p.Message("&f• &SHas &T{0} &S{1}", target.Money.ToString(), Server.Config.Currency);
 				p.Message("&f• &SHas the rank of {0}", group.ColoredName);
+				p.Message("&f• &SHas modified &T{0} &Sblocks", target.TotalModified);
 				p.Message("&f• &SHas logged in &T{0} &Stimes", target.Logins);
 				p.Message("&f• &SFirst login: &T{0}", target.FirstLogin.ToString("yyyy-MM-dd"));
 				p.Message("&f• &SHas spent &T{0} &Son the server", target.TotalTime.Shorten());
+				ItemPerms seeIpPerms = CommandExtraPerms.Find("WhoIs", 1);
+				if (seeIpPerms.UsableBy(p.Rank)) {
+					string ipMsg = target.IP;
+					if (Server.bannedIP.Contains(target.IP)) ipMsg = "&8" + target.IP + ", which is banned";	
+					p.Message("&f• &SThe IP of &T" + ipMsg);
+					if (Server.Config.WhitelistedOnly && Server.whiteList.Contains(name)) p.Message("&f• &SPlayer is &fWhitelisted");
+				}
+
 				if (Server.Config.OwnerName.CaselessEq(target.Name)) p.Message("&f• &SPlayer is the &cServer owner");
+				if (!Group.BannedRank.Players.Contains(target.Name)) return;            
+				string banner, reason, prevRank;
+				DateTime time;
+				Ban.GetBanData(target.Name, out banner, out reason, out time, out prevRank);
+				
+				if (banner != null) p.Message("&f• &SBanned for &T{0} by {1}", reason, p.FormatNick(banner));
+				else p.Message("&f• &SIs banned");
 			}
 			else {
 				Group group = Group.GroupIn(who.name);
@@ -73,6 +92,7 @@ namespace MCGalaxy.Commands.Info
 				p.Message("&S-[ {0} &S({1}) ]-", fullName, who.name);
 				p.Message("&f• &SHas &T{0} &S{1}", who.money.ToString(), Server.Config.Currency);
 				p.Message("&f• &SHas the rank of {0}", group.ColoredName);
+				p.Message("&f• &SHas modified &T{0} &Sblocks, &T{1} &Ssince login", who.TotalModified, who.SessionModified);
 				p.Message("&f• &SHas logged in &T{0} &Stimes", who.TimesVisited);
 				p.Message("&f• &SFirst login: &T{0}", who.FirstLogin.ToString("yyyy-MM-dd"));
 				TimeSpan timeOnline = DateTime.UtcNow - who.SessionStartTime;
@@ -86,7 +106,22 @@ namespace MCGalaxy.Commands.Info
 				bool hasModel = !(who.Model.CaselessEq("humanoid") || who.Model.CaselessEq("human"));
 				if (hasSkin) p.Message("&f• &SHas the skin of &T{0}", who.SkinName);
 				if (hasModel) p.Message("&f• &SHas the model of &T{0}", who.Model);
+				ItemPerms seeIpPerms = CommandExtraPerms.Find("WhoIs", 1);
+				if (seeIpPerms.UsableBy(p.Rank)) {
+					string ipMsg = who.ip;
+					if (Server.bannedIP.Contains(who.ip)) ipMsg = "&8" + who.ip + ", which is banned";
+					p.Message("&f• &SThe IP of &T" + ipMsg);
+					if (Server.Config.WhitelistedOnly && Server.whiteList.Contains(name)) p.Message("&f• &SPlayer is &fWhitelisted");
+				}
+				
 				if (Server.Config.OwnerName.CaselessEq(who.name)) p.Message("&f• &SPlayer is the &cServer owner");
+				if (!Group.BannedRank.Players.Contains(name)) return;            
+				string banner, reason, prevRank;
+				DateTime time;
+				Ban.GetBanData(name, out banner, out reason, out time, out prevRank);
+				
+				if (banner != null) p.Message("&f• &SBanned for &T{0} by {1}", reason, p.FormatNick(banner));
+				else p.Message("&f• &SIs banned");
 			}
 		}
 
