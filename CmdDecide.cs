76d141e88486dc80c482b20377ad5eebfaf219e6
forkiesassds <icanttellyou@memeware.net>
2024-08-09 21:54:38 +0300

    Touchup some older plugins.

--- CmdDecide.cs
+++ CmdDecide.cs
@@ -1,61 +1,34 @@
 //reference System.dll
-
-//	Auto-generated command skeleton class.
-//	Use this as a basis for custom MCGalaxy commands.
-//	File and class should be named a specific way. For example, /update is named 'CmdUpdate.cs' for the file, and 'CmdUpdate' for the class.
-// As a note, MCGalaxy is designed for .NET 4.0
-
-// To reference other assemblies, put a "//reference [assembly filename]" at the top of the file
-//   e.g. to reference the System.Data assembly, put "//reference System.Data.dll"
-
-// Add any other using statements you need after this
 using System;
-using System.Timers;
 using MCGalaxy;
-using MCGalaxy.Events.PlayerEvents;
 using MCGalaxy.Tasks;
-using MCGalaxy.Util;
 
-namespace MCGalaxy 
+namespace VeryPlugins 
 {
 	public class CmdDecide : Command
 	{
-		// The command's name (what you put after a slash to use this command)
-		public override string name { get { return "Decide"; } }
+		public override string name => "Decide";
+		public override string type => CommandTypes.Chat;
+		public override LevelPermission defaultRank => LevelPermission.Guest;
 
-		// Command's shortcut, can be left blank (e.g. "/copy" has a shortcut of "c")
-		public override string shortcut { get { return ""; } }
+		static readonly TimeSpan delay = TimeSpan.FromSeconds(2);
 
-		// Which submenu this command displays in under /Help
-		public override string type { get { return "chat"; } }
-
-		// Whether or not this command can be used in a museum. Block/map altering commands should return false to avoid errors.
-		public override bool museumUsable { get { return true; } }
-
-		// The default rank required to use this command. Valid values are:
-		//   LevelPermission.Guest, LevelPermission.Builder, LevelPermission.AdvBuilder,
-		//   LevelPermission.Operator, LevelPermission.Admin, LevelPermission.Nobody
-		public override LevelPermission defaultRank { get { return LevelPermission.Guest; } }
-
-		// This is for when a player executes this command by doing /Decide
-		//   p is the player object for the player executing the command. 
-		//   message is the arguments given to the command. (e.g. for '/update this', message is "this")
 		public override void Use(Player p, string message)
 		{
-			System.Timers.Timer extraTimer = new System.Timers.Timer(2000);
-			Random random = new Random();
-			string msg = "The magic ball is deciding for " + p.ColoredName + " %Sbetween &f";
-			int i = 0;
-		    var choices = string.Join(" ", message).Split(',');
-		    while (i < choices.Length) 
+			string msg = "The magic ball is deciding for " + p.ColoredName + " &Sbetween &f";
+		    string[] choices = string.Join(" ", message).Split(',');
+		    for (int i = 0; i < choices.Length; i++) 
 		    {
-		    	if (choices[i][0] == ' ') { msg += choices[i].Substring(1, choices[i].Length - 1); }
-				else {msg += choices[i]; }
+		    	if (choices[i][0] == ' ') 
+					msg += choices[i].Substring(1, choices[i].Length - 1);
+				else 
+					msg += choices[i];
+
 		    	if (i == choices.Length - 2) 
 		    	{
 		    		msg += " %Sand &f";
 		    	}
-				else if (i  == choices.Length - 1) 
+				else if (i == choices.Length - 1) 
 				{
 					msg += "%S.";
 				}
@@ -63,22 +36,38 @@ namespace MCGalaxy
 		    	{
 		    		msg += "%S, &f";
 		    	}
-		    	i++;
 		    }
-		    //await ctx.RespondAsync();
-		    Chat.Message(ChatScope.Global, msg, null, Filter8Ball);
-			extraTimer.Start();
-			extraTimer.Elapsed += delegate {
-				extraTimer.Stop();
-				Chat.Message(ChatScope.Global, "The magic ball has decided for " + p.ColoredName + ", choosing &f" + choices[random.Next(0, choices.Length)], null, Filter8Ball);
-			};
+		    Chat.Message(ChatScope.Global, msg, null, (p, arg) => !p.Ignores.EightBall);
+
+			DecideState state;
+			state.p = p;
+			state.choices = choices;
+
+			Server.MainScheduler.QueueOnce(DecideCallback, state, delay);
 		}
 
-		// This is for when a player does /Help Decide
+		static void DecideCallback(SchedulerTask task)
+		{
+			DecideState state = (DecideState)task.State;
+			string[] choices = state.choices;
+			Player p = state.p;
+
+			Random random = new Random();
+
+			Chat.Message(ChatScope.Global, "The magic ball has decided for " + p.ColoredName + ", choosing &f" + choices[random.Next(0, choices.Length)], 
+							null, (p, arg) => !p.Ignores.EightBall);
+
+		}
+
 		public override void Help(Player p)
 		{
 			p.Message("/Decide - Allows you to decide between different options, Usage: %a/decide choice, choice2");
 		}
-		static bool Filter8Ball(Player p, object arg) { return !p.Ignores.EightBall; }
+
+		private struct DecideState
+		{
+			public Player p;
+			public string[] choices;
+		}
 	}
 }
