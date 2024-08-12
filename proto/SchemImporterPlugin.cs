1144ba12eedf42c25b7220169c3b5b2f295dd110
forkiesassds <icanttellyou@memeware.net>
2024-08-13 01:54:48 +0300

    Work in progress .schem importer.
    Doesn't compile as-is using /pcompile

---
+++ proto/SchemImporterPlugin.cs
@@ -0,0 +1,22 @@
+using MCGalaxy;
+using MCGalaxy.Levels.IO;
+
+namespace VeryPlugins
+{
+    public class SchemImporterPlugin : Plugin
+    {
+        public override string name => "SchemImporterPlugin";
+
+        readonly IMapImporter format = new SchemImporter();
+
+        public override void Load(bool auto)
+        {
+            IMapImporter.Formats.Add(format);
+        }
+
+        public override void Unload(bool auto)
+        {
+            IMapImporter.Formats.Remove(format);
+        }
+    }
+}
